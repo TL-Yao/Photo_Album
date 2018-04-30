39d28369b09293a6952dd5954a27bd86dcf74f84
TL-Yao <albert.yao.chn@gmail.com>
2018-04-30 16:06:13 -0400

    add back home button on search result page.

--- app/src/main/java/com/example/apdem/photo_album/SearchResultActivity.java
+++ app/src/main/java/com/example/apdem/photo_album/SearchResultActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -26,6 +27,7 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         album = getIntent().getParcelableExtra(KEY_SEARCH_RESULT);
         System.out.println(album.getPhotoList().size());
@@ -75,5 +77,13 @@ public class SearchResultActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 }
